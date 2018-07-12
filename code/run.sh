503a5fa20910c2587b00a733e7fa4b7a54b8252b
Andy Kavran <akavran@gmail.com>
2018-07-11 21:51:14 -0600

    fixed Figure6c results output directory

--- code/run.sh
+++ code/run.sh
@@ -3,4 +3,4 @@ cd code
 Rscript -e "rmarkdown::render(input = 'Dose_Response_Script.Rmd', output_dir='../results/Dose_Response_Script_Output/', clean = TRUE)"
 Rscript -e "rmarkdown::render(input = 'RNA_Seq_Script.Rmd', output_dir = '../results/RNA_Seq_Script_Output/', clean = TRUE)"
 Rscript -e "rmarkdown::render(input = 'Resistance_and_Sensitivity_Genes_Script.Rmd', output_dir = '../results/Resistance_and_Sensitivity_Genes_Script_Output/', clean = TRUE)"
-Rscript -e "rmarkdown::render(input = 'Figure_6c_Script.Rmd', output_dir='../results/Fig7b_Script_Output/', clean = TRUE)"
+Rscript -e "rmarkdown::render(input = 'Figure_6c_Script.Rmd', output_dir='../results/Fig6c_Script_Output/', clean = TRUE)"
