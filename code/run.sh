d7fef0bb09da211fa03ba1ed54201e492f08ee47
Andy Kavran <akavran@gmail.com>
2018-07-19 16:15:10 -0600

    slight restructuring of results directory

--- code/run.sh
+++ code/run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+set -ex 
 
-Rscript -e "rmarkdown::render(input = 'Dose_Response_Script.Rmd', output_dir='../results/Dose_Response_Script_Output/', clean = TRUE)"
-Rscript -e "rmarkdown::render(input = 'RNA_Seq_Script.Rmd', output_dir = '../results/RNA_Seq_Script_Output/', clean = TRUE)"
-Rscript -e "rmarkdown::render(input = 'Resistance_and_Sensitivity_Genes_Script.Rmd', output_dir = '../results/Resistance_and_Sensitivity_Genes_Script_Output/', clean = TRUE)"
-Rscript -e "rmarkdown::render(input = 'Figure_6c_Script.Rmd', output_dir='../results/Fig6c_Script_Output/', clean = TRUE)"
+mkdir -p ../results/{Dose_Response_Script_Output,RNA_Seq_Script_Output,Resistance_and_Sensitivity_Genes_Script_Output,Fig6c_Script_Output}
+
+
+Rscript -e "rmarkdown::render(input = 'Dose_Response_Script.Rmd', output_dir = '../results', clean = TRUE)"
+Rscript -e "rmarkdown::render(input = 'RNA_Seq_Script.Rmd', output_dir = '../results', clean = TRUE); warnings()"
+Rscript -e "rmarkdown::render(input = 'Resistance_and_Sensitivity_Genes_Script.Rmd', output_dir = '../results', clean = TRUE); warnings()"
+Rscript -e "rmarkdown::render(input = 'Figure_6c_Script.Rmd', output_dir = '../results', clean = TRUE); warnings()"
+
